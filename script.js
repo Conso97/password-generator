7cd5f34f1ea33b53b8446e64416ac22835508b94
Consolata Njeri <conso4u@gmail.com>
2021-05-19 13:24:23 -0700

    added and declared lowercase array on script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Declare global data 
+
+// Declare lowercase array
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// Declare uppercase array
+var uppercase = lowercase.map(function(x){ return x.toUpperCase(); });
+//Declare special array
+
+//Declare numbers array 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
